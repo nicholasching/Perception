c8445f2385114c2829ef50c6ae883b436add7eda
Nickchaching <nicholaslching@gmail.com>
2025-02-22 23:23:51 -0500

    feat: implemented environment variables

--- app/(tabs)/gemini_util.tsx
+++ app/(tabs)/gemini_util.tsx
@@ -1,10 +1,11 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const genAI = new GoogleGenerativeAI("API_KEY");
+const genAI = new GoogleGenerativeAI(process.env.EXPO_PUBLIC_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export async function geminiTest(): Promise<string> {
     let promise = new Promise<string>(async function(resolve) {
         const prompt = "Say Hi!";
+        console.log(process.env.API_KEY);
 
         const result = await model.generateContent(prompt);
 
