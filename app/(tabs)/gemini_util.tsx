2972fe099a922e219a4cb52d3c9baaaf089c8069
Nickchaching <nicholaslching@gmail.com>
2025-02-22 23:32:53 -0500

    chore: removed debug messages

--- app/(tabs)/gemini_util.tsx
+++ app/(tabs)/gemini_util.tsx
@@ -5,7 +5,6 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 export async function geminiTest(): Promise<string> {
     let promise = new Promise<string>(async function(resolve) {
         const prompt = "Say Hi!";
-        console.log(process.env.API_KEY);
 
         const result = await model.generateContent(prompt);
 
